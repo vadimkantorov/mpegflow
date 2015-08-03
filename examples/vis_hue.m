d76befdede674ab7d38d12a0390828be1e695d73
Vadim Kantorov <vadimkantorov@gmail.com>
2015-08-03 14:36:23 +0200

    added hue example images

--- examples/vis_hue.m
+++ examples/vis_hue.m
@@ -1,5 +1,5 @@
 % The script requires the training sequences of the MPI Sintel dataset (http://sintel.is.tue.mpg.de/).
-% flowToColor is originally part of the MPI Sintel's bundler.
+% flowToColor and computeColor are originally part of the MPI Sintel's bundler.
 % The script has originally been used to produce visualizations for https://github.com/vadimkantorov/mpegflow/blob/master/README.md, but you could use it on other videos by adjusting VIDEO_PATH
 
 VIDEO_PATH = 'mpi_sintel_final_alley_1.avi';
@@ -15,7 +15,7 @@ while true
     if isempty(frameIndex_rows_cols)
         break
     end
-        
+
     dxdy = fscanf(f, '%d ', fliplr(frameIndex_rows_cols(2:end)'))';
     flow = cat(3, dxdy(1:size(dxdy, 1) / 2, :), dxdy(size(dxdy, 1) / 2 + 1 : end, :));
     img = flowToColor(flow);
