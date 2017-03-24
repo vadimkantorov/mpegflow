68a4490006942ba707903f8b70fd96fe1fdae1b7
Vadim Kantorov <vadimkantorov@gmail.com>
2017-03-24 11:57:40 +0900

    Update vis_mpeg_flow.sh

--- examples/vis_mpeg_flow.sh
+++ examples/vis_mpeg_flow.sh
@@ -4,4 +4,4 @@ mkdir -p vis_dump
 
 # visualize motion vectors and occupancy info
 mkdir -p vis_dump
-../mpegflow --occupancy mpi_sintel_final_alley_1.avi | ../vis mpi_sintel_final_alley.avi vis_dump
+../mpegflow --occupancy mpi_sintel_final_alley_1.avi | ../vis --occupancy mpi_sintel_final_alley.avi vis_dump
